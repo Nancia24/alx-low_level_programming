ed8ee7cc4c27d47e80ba01fe5ac4b9cb36cb2a8f
Nancia <nanciamwaramba@gmail.com>
2022-12-14 17:27:38 -0800

    task 0

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Return: the length of the string
+ */
+
+void _puts(char *str)
+{
+	for(; *str !='\0'; str++)
+	{
+		_putchar(*str);
+	}
+
+	_putchar('\n');
+}
